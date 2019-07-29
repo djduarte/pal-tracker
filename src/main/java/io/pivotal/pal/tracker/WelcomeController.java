2beab8360ef7a2df10fdc6aaaf68d8e6bfd09801
vagrant <vagrant@pivotallab>
2019-07-29 11:14:07 -0300

    Added a new route

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -10,4 +10,9 @@ public class WelcomeController {
     public String sayHello(){
         return "hello";
     }
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello2";
+    }
 }
